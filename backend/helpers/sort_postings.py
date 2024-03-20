d1a139e3088271bd7adaee27ea3d540e65532d33
Bella Hu <brh76@cornell.edu>
2024-03-20 00:40:43 -0400

    added sorting filtering script

---
+++ backend/helpers/sort_postings.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def sort_by_happiness(lst):
+  return None
+
+def sort_by_company_rating(lst):
+  sorted(lst)
+  return None
+
+def filter_for_location(lst):
+  return None
+
+def filter_for_salary(lst):
+  return None
