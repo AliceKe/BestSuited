d60dd51db79d75de1b29a65bc48520e0f6f7a53d
Alice Ke <aliceke64@gmail.com>
2024-03-21 23:20:57 -0400

    remove company description

--- frontend/bestsuited/src/components/JobPostingCard.jsx
+++ frontend/bestsuited/src/components/JobPostingCard.jsx
@@ -5,7 +5,7 @@ const JobPostingCard = ({ posting }) => {
         <h5 className="card-header">{posting.role}</h5>
         <div className="card-body">
         <h5 className="card-title">{posting.city}, {posting.country}</h5>
-        <p className="card-text">{posting.description}</p>
+        <p className="card-text">Company Rating: {posting.rating}</p>
         <a href="#" className="btn btn-primary">Link to apply</a>
         </div>
         </div>
