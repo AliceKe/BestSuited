8aca54c0110bc289e897569b09babb3259277212
Alice Ke <aliceke64@gmail.com>
2024-03-21 22:58:14 -0400

    add job card

--- frontend/bestsuited/src/App.js
+++ frontend/bestsuited/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import CompanyPostings from './components/CompanyPostings';
 import SearchBar from './components/SearchBar';
+import SortSlider from './components/SortSlider';
 
 function App() {
   return (
     <div className="App">
 
       <SearchBar />
+      <SortSlider/>
       <CompanyPostings />
     </div>
   );
--- frontend/bestsuited/src/components/JobPostingCard.jsx
+++ frontend/bestsuited/src/components/JobPostingCard.jsx
@@ -1,12 +1,13 @@
 const JobPostingCard = (posting) => {
 
     return (
-        <div className="card border-primary mb-3 mt-3">
-            <div className="card-header">Header</div>
-            <div className="card-body text-primary">
-                <h5 className="card-title">Primary card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
+        <div className="card">
+        <h5 className="card-header">Company</h5>
+        <div className="card-body">
+        <h5 className="card-title">Role</h5>
+        <p className="card-text">Location</p>
+        <a href="#" className="btn btn-primary">Link to apply</a>
+        </div>
         </div>
     )
 }
