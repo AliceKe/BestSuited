454e385dedaa8eb63324d7aa45d96469a2182446
Alice Ke <aliceke64@gmail.com>
2024-03-21 23:59:08 -0400

    changing job card

--- frontend/bestsuited/src/components/JobPostingCard.jsx
+++ frontend/bestsuited/src/components/JobPostingCard.jsx
@@ -1,17 +1,15 @@
 const JobPostingCard = ({ posting }) => {
 
     return (
-        <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
-
-        
+        <div class="card" >
+        <div class="card-body">
+            <h5 class="card-title">{posting.role}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{posting.city}, {posting.country}</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+        </div>
     )
 }
 
