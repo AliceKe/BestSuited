13190a522b247599927e9363e837f7252d0b263c
Bella Hu <brh76@cornell.edu>
2024-03-22 02:25:28 -0400

    merge conflict resolve

--- frontend/bestsuited/src/App.js
+++ frontend/bestsuited/src/App.js
@@ -3,11 +3,8 @@ import { useState } from "react"
 import './App.css';
 import CompanyPostings from './components/CompanyPostings';
 import SearchBar from './components/SearchBar';
-<<<<<<< HEAD
 import ExpandedSearchForm from "./components/ExpandedSearchForm";
-=======
 import SortByDropDown from './components/SortBy';
->>>>>>> main
 
 function App() {
   const [postings, setPostings] = useState([])
@@ -17,19 +14,12 @@ function App() {
 
       <SearchBar setPostings={setPostings} />
 
-<<<<<<< HEAD
       <div class="hide">
         <ExpandedSearchForm setPostings={setPostings} />
       </div>
 
       {Object.entries(postings).map(([company, data]) => (<CompanyPostings company={company} data={data} />))}
 
-=======
-      <div>
-        {Object.entries(postings)
-          .map(([company, data]) => (<CompanyPostings company={company} data={data} />))}
-      </div>
->>>>>>> main
 
     </div>
   );
