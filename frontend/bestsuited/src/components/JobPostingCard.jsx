7d70d956559d805e3f73af2308ba4ec4a738beff
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2024-03-22 00:05:44 -0400

    updating bootstrap version

--- frontend/bestsuited/src/components/JobPostingCard.jsx
+++ frontend/bestsuited/src/components/JobPostingCard.jsx
@@ -1,14 +1,17 @@
 const JobPostingCard = ({ posting }) => {
 
     return (
-        <div className="card">
-            <h5 className="card-header">{posting.role}</h5>
-            <div className="card-body">
-                <h5 className="card-title">{posting.city}, {posting.country}</h5>
-                <p className="card-text">{posting.description}</p>
-                <a href="#" className="btn btn-primary">Link to apply</a>
+        <div className="col-md-4">
+            <div className="card mt-1">
+                <h5 className="card-header">{posting.role}</h5>
+                <div className="card-body">
+                    <h5 className="card-title">{posting.city}, {posting.country}</h5>
+                    <p class="text-truncate">{posting.description}</p>
+                    <a href="#" className="btn btn-outline-primary rounded-pill">Link to apply</a>
+                </div>
             </div>
         </div>
+
     )
 }
 
