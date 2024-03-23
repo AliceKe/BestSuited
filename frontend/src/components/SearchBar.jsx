7ac712e9467cede0c7ea2441b9bb567207476144
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2024-03-22 22:25:17 -0400

    arranging files

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import { useState } from 'react';
-import CompanyPostings from './CompanyPostings';
+import React, { useState, useEffect } from 'react';
 
+const SearchBar = ({ setPostings }) => {
+    const [searchQuery, setSearchQuery] = useState('');
 
-const SearchBar = () => {
-    const [postings, setPostings] = useState([])
-
-    const fetchData = async (e) => {
+    const fetchData = async (searchQuery) => {
         try {
-            const response = await fetch(`http://4300showcase.infosci.cornell.edu:5185/regular?q=${e.target.value}`);
+            const response = await fetch(`http://127.0.0.1:5001/jobs?q=${searchQuery}`);
             const data = await response.json();
             setPostings(data.postings);
         } catch (error) {
@@ -16,25 +13,33 @@ const SearchBar = () => {
         }
     }
 
+    const handleInputChange = (e) => {
+        const query = e.target.value;
+        setSearchQuery(query);
+        fetchData(query);
+    }
+
 
 
     return (
         <>
+            <div className="searchbar-container rounded-md">
 
-            <input
-                type="text"
-                placeholder="Search for a job title, company or skills"
-                id="filter-text-val"
-                onChange={fetchData}
-            />
+                <div class="flex input-box center-content">
+                    <img src="/mag.png" alt="Search Icon" className='col-2' ></img>
 
 
-            {Object.entries(postings).map(([company, data]) => (<CompanyPostings company={company} data={data} />))}
-
-
+                    <input
+                        type="text"
+                        placeholder="Search for a job title, company or skills"
+                        id="filter-text-val"
+                        value={searchQuery}
+                        onChange={handleInputChange}
+                        className='col-10 text-center'
+                    />
+                </div >
+            </div>
         </>
-
-
     )
 
 
