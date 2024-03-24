cab1cc2613448d1dd9fe574e44066cb83fe71ea7
Alice Ke <aliceke64@gmail.com>
2024-03-23 22:08:05 -0400

    add groupby function

--- frontend/src/static/script.js
+++ frontend/src/static/script.js
@@ -1,3 +1,23 @@
 const groupPostingsByCompany = (postings) => {
+    let groups = {};
+
+    for (let i = 0; i < postings.length; i++) {
+        let p = postings[i];
+        let company = p["company"];
+
+        if (company in groups) {
+            groups[company]["postings"].push(p);
+        } else {
+            groups[company] = {
+                "name": company,
+                "description": p["description"],
+                "rating": p["rating"],
+                "happiness": p["happiness"],
+                "postings": [p]
+            };
+        }
+    }
+
+    return groups;
 
 }
