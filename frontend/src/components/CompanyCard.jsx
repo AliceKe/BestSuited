bb3d429a4fc55895a02ce09b1fc73ec5bbfc6cbb
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2024-03-25 23:55:08 -0400

    d-block

--- frontend/src/components/CompanyCard.jsx
+++ frontend/src/components/CompanyCard.jsx
@@ -5,57 +5,57 @@ import { Button, Card } from "react-bootstrap/";
 import CompanyRating from "./Rating";
 
 const CompanyCard = ({ companyName, data }) => {
-  const [image, setImage] = useState("");
-  const [imageNotFound, setImageNotFound] = useState(false);
-  const handleImageNotFound = () => {
-    if (!imageNotFound) {
-      setImageNotFound(true);
-      setImage(defaultImage);
-    }
-  };
+    const [image, setImage] = useState("");
+    const [imageNotFound, setImageNotFound] = useState(false);
+    const handleImageNotFound = () => {
+        if (!imageNotFound) {
+            setImageNotFound(true);
+            setImage(defaultImage);
+        }
+    };
 
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
 
-  return (
-    <div className="col-lg-3 col-md-4 col-sm-6 px-3 my-3 zoom">
-      <Card className="bg-light w-90 px-0 py-0">
-        <Card.Body>
-          <Card.Title className="flex">
-            <img
-              src={
-                image ||
-                `https://logo.clearbit.com/${companyName
-                  .replace(/\s+/g, "")
-                  .toLowerCase()}.com?size=20`
-              }
-              onError={handleImageNotFound}
-              alt=""
-            />{" "}
-            {companyName}
-          </Card.Title>
-          <Card.Text>{/* {data.description} */}</Card.Text>
+    return (
+        <div className="col-lg-3 col-md-4 col-sm-6 px-3 my-3 zoom">
+            <Card className="bg-light w-90 px-0 py-0">
+                <Card.Body>
+                    <Card.Title className="flex">
+                        <img
+                            src={
+                                image ||
+                                `https://logo.clearbit.com/${companyName
+                                    .replace(/\s+/g, "")
+                                    .toLowerCase()}.com?size=20`
+                            }
+                            onError={handleImageNotFound}
+                            alt=""
+                        />{" "}
+                        {companyName}
+                    </Card.Title>
+                    <Card.Text>{/* {data.description} */}</Card.Text>
 
-          <div className="d-flex align-items-center ">
-            <CompanyRating value={data.rating} />
-            <Button
-              className="btn-light btn-outline-primary mx-auto"
-              onClick={() => setShow(true)}
-            >
-              See {data.postings.length} postings
-            </Button>
-          </div>
+                    <div className="d-block align-items-center ">
+                        <CompanyRating value={data.rating} />
+                        <Button
+                            className="btn-light btn-outline-primary mx-auto"
+                            onClick={() => setShow(true)}
+                        >
+                            See {data.postings.length} postings
+                        </Button>
+                    </div>
 
-          <PostingsModal
-            show={show}
-            handleClose={handleClose}
-            company={companyName}
-            postings={data.postings}
-          />
-        </Card.Body>
-      </Card>
-    </div>
-  );
+                    <PostingsModal
+                        show={show}
+                        handleClose={handleClose}
+                        company={companyName}
+                        postings={data.postings}
+                    />
+                </Card.Body>
+            </Card>
+        </div>
+    );
 };
 
 export default CompanyCard;
