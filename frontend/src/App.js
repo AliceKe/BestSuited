60a1f762b000c9d78d167add7d557d0dbeda8037
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2024-03-25 23:52:09 -0400

    added sorting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,97 +1,81 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import "./App.css";
-import CompanyCard from "./components/CompanyCard";
-import SearchBar from "./components/SearchBar";
+
+import './App.css';
+import CompanyCard from './components/CompanyCard';
+import SearchBar from './components/SearchBar';
 import DisplayOption from "./components/DisplayOption";
-import { groupPostingsByCompany } from "./static/script";
+import { companiesSortBy, groupPostingsByCompany } from "./static/script";
 import PostingCard from "./components/PostingCard";
 import ResumeUpload from "./components/ResumeUpload";
 
-const sortParams = {
-  Companies: ["Rating", "Name"],
-  "Job Postings": ["Rank", "Role"],
-};
+const sortParams = { "Companies": ["Rating", "Name"], "Job Postings": ["Rank", "Role"] }
+
+
 
 function App() {
-  const [postings, setPostings] = useState([]);
+  const [postings, setPostings] = useState([])
+  const [companiesPostings, setCompaniesPostings] = useState([])
 
-  const [groupBy, setGroupBy] = useState("Companies");
-  const [sortBy, setSortBy] = useState(sortParams.Companies[0]);
 
-  const companyPostings = groupPostingsByCompany(postings);
+  const [groupBy, setGroupBy] = useState("Companies")
+  const [sortBy, setSortBy] = useState(sortParams.Companies[0])
+
+  const handlePostingsUpdate = (data) => {
+    setPostings(data);
+    setCompaniesPostings(groupPostingsByCompany(data));
+  }
+
+  const handleSorting = (val) => {
+    if (["Rating", "Name"].indexOf(val) !== -1) {
+      let tmp = companiesSortBy(companiesPostings, val);
+      setCompaniesPostings(tmp)
+    }
+    setSortBy(val);
+  }
+
 
   return (
     <>
-      <div className="container-fluid bg-light">
+      <div className="container-fluid">
         <div className="top-text">
-          <div className="jumbotron bg-light w-100 mb-3 pt-3 rounded-3">
-            <h1 className="display-2 text-dark text-center py-auto pt-3 poppins-font">
-              BestSuited
-            </h1>
-            <h3 className="display-8 text-center poppins-font">
-              Jobs Tailored for You
-            </h3>
-          </div>
-          <SearchBar setPostings={setPostings} />
+          <h1 className="heading">BESTSUITED</h1>
+          <h2 className="heading ">Jobs Tailored Just For You!</h2>
+
+
+          <SearchBar setPostings={handlePostingsUpdate} />
 
           <div className="row mt-3 justify-content-around">
-            <DisplayOption
-              value={groupBy}
-              setHandler={setGroupBy}
-              variant="outline-primary"
-              type="List"
-              options={Object.keys(sortParams)}
-              cls="rounded-start-pill me-3"
-            />
+            <DisplayOption value={groupBy} setHandler={setGroupBy} variant="outline-primary" type="List" options={Object.keys(sortParams)} cls="rounded-start-pill me-3" />
 
             {/* <FilterAccordion /> */}
-            <ResumeUpload setPostings={setPostings}></ResumeUpload>
-            {groupBy === "Companies" && (
-              <DisplayOption
-                value={sortParams.Companies[0]}
-                setHandler={setSortBy}
-                variant="outline-success"
-                type="Sort By"
-                options={sortParams.Companies}
-                cls="rounded-end-pill ms-3 "
-              />
-            )}
-            {groupBy === "Job Postings" && (
-              <DisplayOption
-                value={sortParams["Job Postings"][0]}
-                setHandler={setSortBy}
-                variant="outline-success"
-                type="Sort By"
-                options={sortParams["Job Postings"]}
-                cls="rounded-end-pill ms-3 "
-              />
-            )}
+            <ResumeUpload setPostings={handlePostingsUpdate}></ResumeUpload>
+            {groupBy === "Companies" && <DisplayOption value={sortBy} setHandler={handleSorting} variant="outline-success" type="Sort By" options={sortParams.Companies} cls="rounded-end-pill ms-3 " />}
+            {groupBy === "Job Postings" && <DisplayOption value={sortBy} setHandler={handleSorting} variant="outline-success" type="Sort By" options={sortParams["Job Postings"]} cls="rounded-end-pill ms-3 " />}
           </div>
 
-          {groupBy === "Job Postings" && (
-            <div class="row mt-1 w-100">
-              {postings.map((posting) => (
-                <PostingCard posting={posting} />
-              ))}
-            </div>
-          )}
 
-          {groupBy === "Companies" && (
+          {
+            groupBy === "Job Postings" &&
             <div class="row mt-1 w-100">
-              {companyPostings.map((companyData, key) => (
-                <CompanyCard
-                  key={key}
-                  companyName={companyData.name}
-                  data={companyData}
-                />
-              ))}
+              {postings.map((posting) => (<PostingCard posting={posting} />))}
             </div>
-          )}
-        </div>
-      </div>
+          }
+
+          {
+            groupBy === "Companies" &&
+            <div class="row mt-1 w-100">
+              {companiesPostings.map((companyData, key) => (<CompanyCard key={key} companyName={companyData.name} data={companyData} />))}
+            </div>
+          }
+
+
+        </div >
+
+      </div >
     </>
   );
 }
+
 
 export default App;
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,66 +3,66 @@ import Spinner from "react-bootstrap/Spinner";
 import { backendUrl } from "../static/script";
 
 const SearchBar = ({ setPostings }) => {
-  const [query, setQuery] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isTyping, setIsTyping] = useState(false);
+    const [query, setQuery] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [isTyping, setIsTyping] = useState(false);
 
-  useEffect(() => {
-    const delay = 500;
-    let timeoutId;
+    useEffect(() => {
+        const delay = 500;
+        let timeoutId;
 
-    if (isTyping) {
-      timeoutId = setTimeout(() => {
-        setIsLoading(true);
-        setIsTyping(false);
-        fetchData();
-      }, delay);
-    }
+        if (isTyping) {
+            timeoutId = setTimeout(() => {
+                setIsLoading(true);
+                setIsTyping(false);
+                fetchData();
+            }, delay);
+        }
 
-    return () => {
-      clearTimeout(timeoutId);
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [query, isTyping, setPostings]);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`${backendUrl.local}/regular?q=${query}`);
+            const data = await response.json();
+            setPostings(data.postings);
+            console.log(data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        } finally {
+            setIsLoading(false);
+        }
     };
-  }, [query, isTyping, setPostings]);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`${backendUrl.remote}/regular?q=${query}`);
-      const data = await response.json();
-      setPostings(data.postings);
-      console.log(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    const handleChange = (e) => {
+        setQuery(e.target.value);
+        setIsTyping(true);
+    };
 
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-    setIsTyping(true);
-  };
+    return (
+        <div className="w-75 flex">
+            <div className="flex w-100 flex-row input-box bg-light center-content">
+                <img src="/mag.png" alt="Search Icon" className="col-2" />
 
-  return (
-    <div className="w-75 flex">
-      <div className="flex w-100 flex-row input-box bg-light center-content">
-        <img src="/mag.png" alt="Search Icon" className="col-2" />
+                <input
+                    type="text"
+                    placeholder="Search for a job title, company or skills"
+                    id="filter-text-val"
+                    onChange={handleChange}
+                    className="col-8 text-center bg-light fs-3 text-dark"
+                />
 
-        <input
-          type="text"
-          placeholder="Search for a job title, company or skills"
-          id="filter-text-val"
-          onChange={handleChange}
-          className="col-8 text-center bg-light fs-3 text-dark"
-        />
-
-        {isTyping && (
-          <div className="col-2">
-            <Spinner animation="border" size="sm" variant="primary" />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+                {isTyping && (
+                    <div className="col-2">
+                        <Spinner animation="border" size="sm" variant="primary" />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default SearchBar;
