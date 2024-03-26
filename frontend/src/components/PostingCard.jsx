2239c9d69ffa16ae4f397f19492ab91fe0cb8ba2
Alice Ke <aliceke64@gmail.com>
2024-03-26 10:27:03 -0400

    add zoom hover to job card

--- frontend/src/components/PostingCard.jsx
+++ frontend/src/components/PostingCard.jsx
@@ -1,56 +1,51 @@
 import { useState } from "react";
-import defaultImage from '../images/favicon.png';
+import defaultImage from "../images/favicon.png";
 import PostingsModal from "./PostingsModal";
-import { Button, Card } from 'react-bootstrap/';
+import { Button, Card } from "react-bootstrap/";
 import CompanyRating from "./Rating";
 
-
 const PostingCard = ({ posting }) => {
-    const [image, setImage] = useState("")
-    const [imageNotFound, setImageNotFound] = useState(false);
+  const [image, setImage] = useState("");
+  const [imageNotFound, setImageNotFound] = useState(false);
 
-    const handleImageNotFound = () => {
-        if (!imageNotFound) {
-            setImageNotFound(true);
-            setImage(defaultImage);
-        }
+  const handleImageNotFound = () => {
+    if (!imageNotFound) {
+      setImageNotFound(true);
+      setImage(defaultImage);
     }
+  };
 
+  return (
+    <div className="col-md-3 col-sm-6 px-3 my-3 zoom">
+      <Card className="bg-light w-90 px-0 py-0 hover-shadow shadow-sm">
+        <Card.Body>
+          <Card.Title className="flex">
+            <img
+              src={
+                image ||
+                `https://logo.clearbit.com/${posting.company
+                  .replace(/\s+/g, "")
+                  .toLowerCase()}.com?size=20`
+              }
+              onError={handleImageNotFound}
+              alt=""
+            />{" "}
+            {posting.role}
+          </Card.Title>
+          <Card.Text>{/* {data.description} */}</Card.Text>
 
+          <div className="d-flex align-items-center justify-content-between">
+            <p>{posting["salary range"]}</p>
+            <p>
+              {posting.city}, {posting.country}
+            </p>
 
-    return (
-
-        <div className="col-md-3 col-sm-6 px-3 my-3 hover-zoom">
-
-            <Card className="bg-light w-90 px-0 py-0">
-
-                <Card.Body>
-                    <Card.Title className="flex" >
-                        <img src={image || `https://logo.clearbit.com/${posting.company.replace(/\s+/g, '').toLowerCase()}.com?size=20`} onError={handleImageNotFound}
-                            alt="" /> {posting.role}
-                    </Card.Title>
-                    <Card.Text>
-                        {/* {data.description} */}
-                    </Card.Text>
-
-                    <div className="d-flex align-items-center justify-content-between">
-                        <p>
-                            {posting["salary range"]}
-                        </p>
-                        <p>
-                            {posting.city}, {posting.country}
-                        </p>
-
-                        {/* <Button className="btn-light btn-outline-primary" onClick={() => setShow(true)}>Apply here!</Button> */}
-                    </div>
-
-                </Card.Body>
-            </Card>
-        </div>
-
-
-
-    )
-}
+            {/* <Button className="btn-light btn-outline-primary" onClick={() => setShow(true)}>Apply here!</Button> */}
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
 export default PostingCard;
