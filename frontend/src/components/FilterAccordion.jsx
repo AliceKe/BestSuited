acbc4d6b8a94fc06467b5d949edf65af9bef4dcc
Alice Ke <aliceke64@gmail.com>
2024-04-29 00:10:09 -0400

    add filter data

--- frontend/src/components/FilterAccordion.jsx
+++ frontend/src/components/FilterAccordion.jsx
@@ -1,34 +1,67 @@
-import { Accordion, Button } from 'react-bootstrap';
-import { useAccordionButton } from 'react-bootstrap/AccordionButton';
-import Card from 'react-bootstrap/Card';
-import ExpandedSearchForm from './ExpandedSearchForm';
-
+import { Accordion, Button } from "react-bootstrap";
+import { useAccordionButton } from "react-bootstrap/AccordionButton";
+import Card from "react-bootstrap/Card";
+import ExpandedSearchForm from "./ExpandedSearchForm";
 
 function CustomToggle({ eventKey, setExpandTextSearch }) {
-    const decoratedOnClick = useAccordionButton(eventKey, () =>
-        setExpandTextSearch(false)
-    );
+  const decoratedOnClick = useAccordionButton(eventKey, () =>
+    setExpandTextSearch(false)
+  );
 
-    return (
-        <Button className='w-100 mr-3 text-dark' variant="warning" onClick={decoratedOnClick}> Filter</Button>
-    );
+  return (
+    <Button
+      className="w-100 mr-3 text-dark"
+      variant="warning"
+      onClick={decoratedOnClick}
+    >
+      {" "}
+      Filter
+    </Button>
+  );
 }
 
+function CollapseContent({ eventKey }) {
+  const decoratedOnClick = useAccordionButton(eventKey, () => {});
 
-
-const FilterAccordion = ({ setFilters, applyFilters, updateFilteredPostings, setExpandTextSearch }) => {
-    return (
-        <Accordion defaultActiveKey={null} className=''>
-            <div className="flexflex-row justify-space-around">
-                <CustomToggle setExpandTextSearch={setExpandTextSearch} eventKey="0"></CustomToggle>
-            </div>
-
-            <Accordion.Collapse eventKey="0">
-                <Card.Body><ExpandedSearchForm setFilters={setFilters} applyFilters={applyFilters} updateFilteredPostings={updateFilteredPostings} /></Card.Body>
-            </Accordion.Collapse>
-        </Accordion>
-    );
+  return (
+    <Button
+      className="w-50 ml-3"
+      variant="outline-info"
+      onClick={decoratedOnClick}
+    >
+      Apply Filter
+    </Button>
+  );
 }
 
+const FilterAccordion = ({
+  setFilters,
+  applyFilters,
+  updateFilteredPostings,
+  setExpandTextSearch,
+}) => {
+  return (
+    <Accordion defaultActiveKey={null} className="">
+      <div className="flexflex-row justify-space-around">
+        <CustomToggle
+          setExpandTextSearch={setExpandTextSearch}
+          eventKey="0"
+        ></CustomToggle>
+      </div>
+
+      <Accordion.Collapse eventKey="0">
+        <>
+          <Card.Body>
+            <ExpandedSearchForm
+              setFilters={setFilters}
+              applyFilters={applyFilters}
+              updateFilteredPostings={updateFilteredPostings}
+            />
+          </Card.Body>
+        </>
+      </Accordion.Collapse>
+    </Accordion>
+  );
+};
 
 export default FilterAccordion;
--- frontend/src/pages/Playground.jsx
+++ frontend/src/pages/Playground.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react"
+import { useState } from "react";
 
-
-import CompanyCard from '../components/CompanyCard';
-import SearchBar from '../components/SearchBar';
+import CompanyCard from "../components/CompanyCard";
+import SearchBar from "../components/SearchBar";
 import DisplayOption from "../components/DisplayOption";
 import { companiesSortBy, groupPostingsByCompany } from "../static/script";
 import PostingCard from "../components/PostingCard";
@@ -10,114 +9,156 @@ import ResumeUpload from "../components/ResumeUpload";
 import FilterAccordion from "../components/FilterAccordion";
 import PostingsModal from "../components/PostingsModal";
 
-const sortParams = { "Companies": ["Default", "Rating", "Name"], "Job Postings": ["Rank", "Role"] }
-
+const sortParams = {
+  Companies: ["Default", "Rating", "Name"],
+  "Job Postings": ["Rank", "Role"],
+};
 
 function Playground() {
-    const [originalPostings, setOriginalPostings] = useState([])
-    const [postings, setPostings] = useState([])
-    const [companiesPostings, setCompaniesPostings] = useState([])
+  const [originalPostings, setOriginalPostings] = useState([]);
+  const [postings, setPostings] = useState([]);
+  const [companiesPostings, setCompaniesPostings] = useState([]);
 
+  const [groupBy, setGroupBy] = useState("Companies");
+  const [sortBy, setSortBy] = useState(sortParams.Companies[0]);
 
-    const [groupBy, setGroupBy] = useState("Companies")
-    const [sortBy, setSortBy] = useState(sortParams.Companies[0])
+  const [expandTextSearch, setExpandTextSearch] = useState(false);
 
-    const [expandTextSearch, setExpandTextSearch] = useState(false);
+  const [filters, setFilters] = useState({ country: [], role: [], skills: [] });
 
-    const [filters, setFilters] = useState({ "country": [], "role": [], "skills": [] })
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
 
+  const handlePostingsUpdate = (data) => {
+    setPostings(data);
+    setOriginalPostings(data);
+    setCompaniesPostings(groupPostingsByCompany(data));
+    // handleSorting(sortBy);
+  };
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-
-
-    const handlePostingsUpdate = (data) => {
-        setPostings(data);
-        setOriginalPostings(data)
-        setCompaniesPostings(groupPostingsByCompany(data));
-        // handleSorting(sortBy);
+  const handleSorting = (val) => {
+    if (["Rating", "Name"].indexOf(val) !== -1) {
+      let tmp = companiesSortBy(companiesPostings, val);
+      setCompaniesPostings(tmp);
     }
+    setSortBy(val);
+  };
 
-    const handleSorting = (val) => {
-        if (["Rating", "Name"].indexOf(val) !== -1) {
-            let tmp = companiesSortBy(companiesPostings, val);
-            setCompaniesPostings(tmp)
+  const updateFilteredPostings = (filteredData) => {};
+
+  const applyFilters = () => {
+    console.log(filters);
+    let tmpPostings = [];
+    console.log(postings[0]);
+    for (let posting of postings) {
+      for (let [filterKey, filterValue] of Object.entries(filters)) {
+        if (filterValue.length !== 0) {
+          if (filterValue.includes(posting[filterKey])) {
+            tmpPostings.push(posting);
+          }
         }
-        setSortBy(val);
+      }
     }
+    console.log(tmpPostings);
+    setPostings(tmpPostings);
+    setCompaniesPostings(groupPostingsByCompany(tmpPostings));
+  };
 
-    const applyFilters = () => {
-        console.log(filters)
-        let tmpPostings = []
-        console.log(postings[0])
-        for (let posting of originalPostings) {
-            for (let [filterKey, filterValue] of Object.entries(filters)) {
-                if (filterValue.length !== 0) {
-                    if (filterValue.includes(posting[filterKey])) {
-                        tmpPostings.push(posting);
-                    }
-                }
-            }
-        }
-        setPostings(tmpPostings);
-        setCompaniesPostings(groupPostingsByCompany(tmpPostings));
-    }
-
-    return (
-        <>
-            <h2 className="display-2 text-light text-center py-auto pt-3 poppins-font">
-                BestSuited
-            </h2>
-            {/* <h5 className="display-8 text-light text-center poppins-font">
+  return (
+    <>
+      <h2 className="display-2 text-light text-center py-auto pt-3 poppins-font">
+        BestSuited
+      </h2>
+      {/* <h5 className="display-8 text-light text-center poppins-font">
                 Jobs Tailored for You
             </h5> */}
 
-            <div className="w-100">
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                    <div className="row z-3 w-100 flex-column align-items-center justify-content-around" style={{ "height": "15%", "marginTop": "0" }}>
-                        <SearchBar setPostings={handlePostingsUpdate} expandTextSearch={expandTextSearch} setExpandTextSearch={setExpandTextSearch} />
-                        <div className="w-50 d-flex mt-3">
-                            <DisplayOption value={groupBy} setHandler={setGroupBy} variant="primary" type="List" options={Object.keys(sortParams)} cls="rounded-start-pill me-3" />
-                            <ResumeUpload setPostings={handlePostingsUpdate}></ResumeUpload>
-                            {groupBy === "Companies" && <DisplayOption value={sortBy} setHandler={handleSorting} variant="success" type="Sort By" options={sortParams.Companies} cls="rounded-end-pill ms-3 " />}
-                            {groupBy === "Job Postings" && <DisplayOption value={sortBy} setHandler={handleSorting} variant="success" type="Sort By" options={sortParams["Job Postings"]} cls="rounded-end-pill ms-3 " />}
-                        </div>
+      <div className="w-100">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <div
+            className="row z-3 w-100 flex-column align-items-center justify-content-around"
+            style={{ height: "15%", marginTop: "0" }}
+          >
+            <SearchBar
+              setPostings={handlePostingsUpdate}
+              expandTextSearch={expandTextSearch}
+              setExpandTextSearch={setExpandTextSearch}
+            />
+            <div className="w-50 d-flex mt-3">
+              <DisplayOption
+                value={groupBy}
+                setHandler={setGroupBy}
+                variant="primary"
+                type="List"
+                options={Object.keys(sortParams)}
+                cls="rounded-start-pill me-3"
+              />
+              <ResumeUpload setPostings={handlePostingsUpdate}></ResumeUpload>
+              {groupBy === "Companies" && (
+                <DisplayOption
+                  value={sortBy}
+                  setHandler={handleSorting}
+                  variant="success"
+                  type="Sort By"
+                  options={sortParams.Companies}
+                  cls="rounded-end-pill ms-3 "
+                />
+              )}
+              {groupBy === "Job Postings" && (
+                <DisplayOption
+                  value={sortBy}
+                  setHandler={handleSorting}
+                  variant="success"
+                  type="Sort By"
+                  options={sortParams["Job Postings"]}
+                  cls="rounded-end-pill ms-3 "
+                />
+              )}
+            </div>
 
-                        <div className="d-flex  align-items-center justify-content-center">
-                            <FilterAccordion applyFilters={applyFilters} setFilters={setFilters} setExpandTextSearch={setExpandTextSearch} />
-                        </div>
-                    </div>
+            <div className="d-flex w-100 align-items-center justify-content-center">
+              <FilterAccordion
+                applyFilters={applyFilters}
+                setFilters={setFilters}
+                setExpandTextSearch={setExpandTextSearch}
+              />
+            </div>
+          </div>
 
-                    <div className="w-100 d-flex align-items-center justify-content-center"  >
-                        {
-                            groupBy === "Job Postings" &&
-                            <>
-                                <table class="table border border-5 mt-3 shadow-lg w-50">
-                                    <tbody>
-                                        {postings.map((posting) => (<PostingCard posting={posting} />))}
-                                    </tbody>
-                                </table>
-                            </>
-                        }
+          <div className="w-100 d-flex align-items-center justify-content-center">
+            {groupBy === "Job Postings" && (
+              <>
+                <table class="table border border-5 mt-3 shadow-lg w-50">
+                  <tbody>
+                    {postings.map((posting) => (
+                      <PostingCard posting={posting} />
+                    ))}
+                  </tbody>
+                </table>
+              </>
+            )}
 
-                        {
-                            groupBy === "Companies" &&
-                            <>
-                                <table class="table border border-5 mt-3 shadow-lg w-50">
-                                    <tbody>
-                                        {companiesPostings.map((companyData, key) => (<CompanyCard key={key} companyName={companyData.name} data={companyData} setShow={setShow} />))}
-                                    </tbody>
-                                </table>
-                            </>
-                        }
-
-                    </div>
-                </div >
-
-            </div >
-        </>
-    );
+            {groupBy === "Companies" && (
+              <>
+                <table class="table border border-5 mt-3 shadow-lg w-50">
+                  <tbody>
+                    {companiesPostings.map((companyData, key) => (
+                      <CompanyCard
+                        key={key}
+                        companyName={companyData.name}
+                        data={companyData}
+                        setShow={setShow}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
 
 export default Playground;
